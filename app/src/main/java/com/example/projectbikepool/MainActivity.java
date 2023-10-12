8fcab987626cf8b126794a9a9b2b41d0854274dc
Atharv <atharvkale22@gmail.com>
2023-10-13 00:23:53 +0530

    Login Page Done with Authentication

--- app/src/main/java/com/example/projectbikepool/MainActivity.java
+++ app/src/main/java/com/example/projectbikepool/MainActivity.java
@@ -1,14 +1,96 @@
 package com.example.projectbikepool;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    String email_regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+    FirebaseFirestore db;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    EditText name,email,mobile_no,pass;
+    String NAME,EMAIL,MOBILE_NO,Password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name=(EditText) findViewById(R.id.uname);
+        email=(EditText) findViewById(R.id.signupemail);
+        mobile_no=(EditText) findViewById(R.id.signup_mobile_no);
+        pass=(EditText) findViewById(R.id.signpassword);
+
+        db = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+    }
+    public void sign_up(View view){
+        NAME=name.getText().toString();
+        EMAIL=email.getText().toString();
+        MOBILE_NO=mobile_no.getText().toString();
+        Password=pass.getText().toString();
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("Name", NAME);
+        user.put("Email", EMAIL);
+        user.put("MobileNo", MOBILE_NO);
+        user.put("Password", Password);
+
+        if (!EMAIL.matches(email_regex)) {
+            email.setError("Enter correct Email");
+        }
+        else if(!(MOBILE_NO.length()==10)){
+            mobile_no.setError("Enter Correct Mobile Number");
+        }
+        else if (Password.length() < 6) {
+            pass.setError("Password Length must be greater than 5");
+        }
+        else{
+            db.collection("Users")
+                    .document(""+EMAIL)
+                    .set(user).
+                    addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(MainActivity.this, "Created Account Successfully", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, "An Error Occurred", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            firebaseAuth.createUserWithEmailAndPassword(EMAIL, Password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Toast.makeText(MainActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(MainActivity.this,MainPage.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, "Can't Register!An Error Occurred", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/projectbikepool/MainPage.java
+++ app/src/main/java/com/example/projectbikepool/MainPage.java
@@ -1,5 +1,6 @@
 package com.example.projectbikepool;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -7,9 +8,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainPage extends AppCompatActivity {
-
+    TextInputEditText uname, pass;
+    String email_regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    FirebaseFirestore db;
+    public static String get_DBName,get_DBEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +34,66 @@ public class MainPage extends AppCompatActivity {
 
       RelativeLayout signup = findViewById(R.id.gotosignup);
 
-
+        uname = (TextInputEditText) findViewById(R.id.mail);
+        pass = (TextInputEditText) findViewById(R.id.pass);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+     //   assert firebaseUser != null;
+    //    String id=firebaseUser.getUid();
+        db=FirebaseFirestore.getInstance();
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainPage.this,MainActivity.class));
             }
         });
+    }
+
+    public void Login(View view) {
+        String email = uname.getText().toString();
+        String psw = pass.getText().toString();
+
+        if (!email.matches(email_regex)) {
+            uname.setError("Enter correct Email");
+        } else if (psw.length() < 6) {
+            pass.setError("Password Length must be greater than 5");
+        } else {
+
+            firebaseAuth.signInWithEmailAndPassword(email,psw).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+
+                    DocumentReference documentReference=db.collection("Users").document(email);
+                    documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            if(documentSnapshot.exists()){
+                                get_DBName=documentSnapshot.getString("Name");
+                                get_DBEmail=documentSnapshot.getString("Email");
+
+
+                                Toast.makeText(getApplicationContext(), "Login in Successful", Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(MainPage.this, Home_page.class);
+//                                intent.putExtra("NAME",get_DBName);
+//                                intent.putExtra("Email",get_DBEmail);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), "Login in Failed\nPlease Recheck Username and Password", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getApplicationContext(), "Login in Failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
