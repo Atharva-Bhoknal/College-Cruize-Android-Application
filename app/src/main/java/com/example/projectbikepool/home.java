fb6b98172307220f7ff6932200be0272e77b64ae
Atharva <atharva4b@gmail.coim>
2023-10-22 14:12:33 +0530

    Home Page and Profile page 2.0 Submission

---
+++ app/src/main/java/com/example/projectbikepool/home.java
@@ -0,0 +1,16 @@
+package com.example.projectbikepool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
